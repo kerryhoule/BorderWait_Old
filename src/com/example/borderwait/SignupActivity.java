15e9bc254ecea8fc149c1550517170a16810178a
Kerry Houle <khoule@gmail.com>
2014-04-23 20:44:30 -0500

    Added parse to signup page

--- src/com/example/borderwait/SignupActivity.java
+++ src/com/example/borderwait/SignupActivity.java
@@ -1,25 +1,93 @@
 package com.example.borderwait;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignupActivity extends FragmentActivity {
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class SignupActivity extends Activity {
+	
+	//Variables to hold references to objects on the page
+	EditText etName, etEmail, etPassword, etConfirm;
+	Button btnOk, btnClear, btnCancel;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_signup);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		//Get parse ready to go
+		Parse.initialize(this, "4rMpAqbh9hdSyIRcvefxtInZtaPz3Ef7K8tGaEWS", "MXxegBkLTfCyM4kSHBlgQSj6y54ZaFtdjmV20SV2");
+		
+		//Get references to all the edit texts and buttons
+		etName = (EditText)findViewById(R.id.etSignupName);
+		etEmail = (EditText)findViewById(R.id.etSignupEmail);
+		etPassword = (EditText)findViewById(R.id.etSignupPass);
+		etConfirm = (EditText)findViewById(R.id.etSignupConfirm);
+		btnOk = (Button)findViewById(R.id.btnSignupOk);
+		btnClear = (Button)findViewById(R.id.btnSignupClear);
+		btnCancel = (Button)findViewById(R.id.btnSignupCancel);
+		
+		//Build listeners for buttons
+		btnOk.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				//Check if all fields have data in them, if not show error for them
+				if (etName.getText().toString().length() > 0 && etEmail.getText().toString().length() > 0 && etPassword.getText().toString().length() > 0 && etConfirm.getText().toString().length() > 0)
+				{
+					//Check that passwords match
+					if (etPassword.getText().toString().equals(etConfirm.getText().toString()))
+					{
+						//All fields have data entered, make a user!
+						ParseObject user = new ParseObject("User");
+						
+						//Fill out the information
+						user.put("name", etName.getText().toString());
+						user.put("email", etEmail.getText().toString());
+						user.put("password", etPassword.getText().toString());
+						
+						//Save the user to the DB
+						user.saveInBackground();
+					}
+					else
+					{
+						//Show error with passwords
+						Toast.makeText(SignupActivity.this, "Passwords do not match.", Toast.LENGTH_LONG).show();
+					}
+				}
+				else
+				{
+					//Some fields weren't filled out
+					Toast.makeText(SignupActivity.this, "Something wasnt filled out, will find out after", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+		
+		btnClear.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				//Clear all fields
+				etName.setText("");
+				etEmail.setText("");
+				etPassword.setText("");
+				etConfirm.setText("");
+			}
+		});
+		
+		btnCancel.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				onBackPressed();
+			}
+		});
 	}
 
 	@Override
@@ -41,22 +109,4 @@ public class SignupActivity extends FragmentActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_signup,
-					container, false);
-			return rootView;
-		}
-	}
-
 }
