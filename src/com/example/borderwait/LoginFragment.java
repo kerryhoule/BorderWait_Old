b7c6f4623a24e302a61bbf5a6e834d837d735e64
Kerry Houle <khoule@gmail.com>
2014-04-23 19:11:20 -0500

    Got click method to run in fragment

--- src/com/example/borderwait/LoginFragment.java
+++ src/com/example/borderwait/LoginFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,6 +16,7 @@ public class LoginFragment extends Fragment {
 	//Variables to hold references to objects on the page
 	EditText etEmail, etPassword;
 	CheckBox cbxRemember;
+	Button btnSignUp, btnSignIn;
 	
 	public LoginFragment() {
 	}
@@ -28,37 +31,42 @@ public class LoginFragment extends Fragment {
 		etEmail = (EditText)rootView.findViewById(R.id.etEmail);
 		etPassword = (EditText)rootView.findViewById(R.id.etPassword);
 		cbxRemember = (CheckBox)rootView.findViewById(R.id.cbxRemember);
+		btnSignUp = (Button)rootView.findViewById(R.id.btnsignup);
+		btnSignIn = (Button)rootView.findViewById(R.id.btnSignin);
+		
+		//Setup Listeners
+		btnSignIn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				//Check if user input an email or password
+				if (!(etEmail.getText().toString().length() == 0 && etPassword.getText().toString().length() == 0))
+				{
+					//User input something in both fields, check DB for user
+					
+					
+					//Replace the fragment thats already there
+					getActivity().getSupportFragmentManager().beginTransaction()
+					.replace(R.id.userInfoContainer, new LoggedinFragment()).commit();
+				}
+				else
+				{
+					//User did not input something, check both fields
+					if (etEmail.getText().toString().length() > 0)
+					{
+						//Email has data in it, password must not have any data
+						Toast.makeText(getActivity(), "You must enter a password",
+				                Toast.LENGTH_LONG).show();
+					}
+					else
+					{
+						//Email is empty might as well tell them to fix it regardless if password is filled or not
+						Toast.makeText(getActivity(), "You must enter an email address",
+				                Toast.LENGTH_LONG).show();
+					}
+				}
+			}
+		});
 		
 		return rootView;
-	}
-	
-	public void signIn(View v)
-	{
-		//Check if user input an email or password
-		if (!(etEmail.getText().toString().length() == 0 && etPassword.getText().toString().length() == 0))
-		{
-			//User input something in both fields, check DB for user
-			
-			
-			//Replace the fragment thats already there
-			getActivity().getSupportFragmentManager().beginTransaction()
-			.replace(R.id.userInfoContainer, new LoggedinFragment()).commit();
-		}
-		else
-		{
-			//User did not input something, check both fields
-			if (etEmail.getText().toString().length() > 0)
-			{
-				//Email has data in it, password must not have any data
-				Toast.makeText(getActivity(), "You must enter a password",
-		                Toast.LENGTH_LONG).show();
-			}
-			else
-			{
-				//Email is empty might as well tell them to fix it regardless if password is filled or not
-				Toast.makeText(getActivity(), "You must enter an email address",
-		                Toast.LENGTH_LONG).show();
-			}
-		}
 	}
 }
